8c11e1e52e1ff42b5de4b84ce51ea7b1cad469f1
sampoulin <spoulin1234@gmail.com>
2019-01-24 20:44:29 -0500

    Initial commit

---
+++ engine.py
@@ -0,0 +1,64 @@
+import tcod as libtcod
+from input_handlers import handle_keys
+
+def main():
+    #sets varaibles for screen width and height (used later on)
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    #sets the font of the console to arial10x10.png
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE
+        | libtcod.FONT_LAYOUT_TCOD)
+
+    #creates a non-fullscreen window with the width and height defined earlier
+    #and the title of "Tutorial"
+    libtcod.console_init_root(screen_width, screen_height, "Tutorial", False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    #main game loop
+    while not libtcod.console_is_window_closed():
+        #updates the key and mouse variables with any key or mouse events
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        #sets the foreground color of console 0 to white
+        libtcod.console_set_default_foreground(con, libtcod.white)
+
+        #puts an @ character at the middle of the screen with no background
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+        libtcod.console_flush()
+
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ input_handlers.py
@@ -0,0 +1,29 @@
+import tcod as libtcod
+
+def handle_keys(key):
+    #movement keys
+    if key.vk == libtcod.KEY_KP8:
+        return {'move' : (0, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move' : (1, -1)}
+    elif key.vk == libtcod.KEY_KP6:
+        return {'move' : (1, 0)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move' : (1, 1)}
+    elif key.vk == libtcod.KEY_KP2:
+        return {'move' : (0, 1)}
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move' : (-1, 1)}
+    elif key.vk == libtcod.KEY_KP4:
+        return {'move' : (-1, 0)}
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move' : (-1, -1)}
+
+    #toggle fullscreen
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        return {'fullscreen' : True}
+
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit' : True}
+
+    return {}
