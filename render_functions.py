18dc6b3685ce5fb7254f0e1de1f0c0601eea29bc
sampoulin <spoulin1234@gmail.com>
2019-01-28 14:28:08 -0500

    render_functions uses game_map to draw map tiles

--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,16 @@
 import tcod as libtcod
 
-def render_all(con, entities, screen_width, screen_height):
+def render_all(con, entities, game_map, screen_width, screen_height, colors):
+
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
+
+            if wall:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+
     for entity in entities:
         draw_entity(con, entity)
 
@@ -16,4 +26,3 @@ def draw_entity(con, entity):
 
 def clear_entity(con, entity):
     libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
-    
