473bf02d8ae6f30fdf084121cbb0aedb21f4aa7e
sampoulin <spoulin1234@gmail.com>
2019-01-28 13:50:00 -0500

    engine updated to use entity

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 import tcod as libtcod
+
+from entity import Entity
 from input_handlers import handle_keys
 
 def main():
@@ -6,8 +8,9 @@ def main():
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     #sets the font of the console to arial10x10.png
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE
@@ -47,8 +50,7 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            player.move(dx, dy)
 
         if exit:
             return True
