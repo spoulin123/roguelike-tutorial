cf71200e8b6a61bcda9e5360714466c63ab344c1
sampoulin <spoulin1234@gmail.com>
2019-01-28 21:27:25 -0500

    rectangle updated with center and intersect methods

--- map_objects/rectangle.py
+++ map_objects/rectangle.py
@@ -5,3 +5,12 @@ class Rect:
         self.x2 = x + w
         self.y1 = y
         self.y2 = y + h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+            self.y1 <= self.y1 and self.y2 >= other.y1)
