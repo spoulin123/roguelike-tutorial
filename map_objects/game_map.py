b3b38fdd678b624cb9eced03a6a5f4ede79557c9
sampoulin <spoulin1234@gmail.com>
2019-01-28 21:28:20 -0500

    game_map given random generation capabilities and engine updated to utilize new capabilities

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,6 @@
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from random import randint
 
 class GameMap:
     def __init__(self, width, height):
@@ -11,14 +12,40 @@ class GameMap:
         tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
         return tiles
 
-    def make_map(self):
-        room1 = Rect(20, 15, 10, 15)
-        room2 = Rect(30, 15, 10, 15)
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+        rooms = []
+        num_rooms = 0
+        for r in range(max_rooms):
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h - 1)
 
-        self.create_room(room1)
-        self.create_room(room2)
+            new_room = Rect(x, y, w, h)
 
-        self.create_h_tunnel(25, 40, 23)
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                self.create_room(new_room)
+
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                    if randint(0, 1) == 1:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+                    else:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+
+            rooms.append(new_room)
+            num_rooms += 1
 
     def create_room(self, room):
         for x in range(room.x1 + 1, room.x2):
