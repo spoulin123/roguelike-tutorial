5a224a6ae6aff5ad7c12abe4a28aa3208d853b2a
sampoulin <spoulin1234@gmail.com>
2019-01-28 20:56:23 -0500

    added tunnel methods

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -18,11 +18,23 @@ class GameMap:
         self.create_room(room1)
         self.create_room(room2)
 
+        self.create_h_tunnel(25, 40, 23)
+
     def create_room(self, room):
         for x in range(room.x1 + 1, room.x2):
             for y in range(room.y1 + 1, room.y2):
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
