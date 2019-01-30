a5ba9a879f0baa1aa710d5aa453b742e324b1adc
sampoulin <spoulin1234@gmail.com>
2019-01-30 10:30:46 -0500

    fov added

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from entity import Entity
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
+from fov_functions import initialize_fov, recompute_fov
 
 def main():
     #sets varaibles for screen width and height (used later on)
@@ -16,9 +17,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithim = 0
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
@@ -38,6 +45,10 @@ def main():
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
@@ -45,6 +56,9 @@ def main():
     while not libtcod.console_is_window_closed():
         #updates the key and mouse variables with any key or mouse events
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithim)
 
         render_all(con, entities, game_map, screen_width, screen_height, colors)
 
@@ -62,6 +76,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
