b23c13674022c284f37d273ef23ef57c0cb9f6de
sampoulin <spoulin1234@gmail.com>
2019-01-31 06:03:38 -0500

    fixed call to render_all() in engine

--- engine.py
+++ engine.py
@@ -60,7 +60,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithim)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
 
         libtcod.console_flush()
 
