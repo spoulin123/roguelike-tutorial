ba64dd4a1c1552275106036c68be9dd6fe7fe976
sampoulin <spoulin1234@gmail.com>
2019-01-31 13:01:03 -0500

    entity updated to use fighter and ai components

--- entity.py
+++ entity.py
@@ -1,12 +1,20 @@
 class Entity:
 
-    def __init__ (self, x, y, char, color, name, blocks = False):
+    def __init__ (self, x, y, char, color, name, blocks = False, fighter = None, ai = None):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
         self.name = name
         self.blocks = blocks
+        self.fighter = fighter
+        self.ai = ai
+
+        if self.fighter:
+            self.fighter.owner = self
+
+        if self.ai:
+            self.ai.owner = self
 
     def move(self, dx, dy):
         self.x += dx
